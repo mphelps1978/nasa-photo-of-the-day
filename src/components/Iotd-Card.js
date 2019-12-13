2dface251de106237c4fb735216efa1752b14061
mphelps1978 <phelps.michael@live.com>
2019-12-12 23:44:13 -0600

    One last tweak to the layout and link button

--- src/components/Iotd-Card.js
+++ src/components/Iotd-Card.js
@@ -12,15 +12,14 @@ import {
 
 const CardWrapper = styled.div`
   margin-left: 800px;
-  padding-top: 100px;
-  margin-bottom: 100%;
+  padding-top: 20px;
+  ${"" /* margin-bottom: 100%; */}
 `;
 
 const TitleWrapper = styled.h2`
   color: #333333;
   font-size: 3rem;
   ${"" /* text-align: center; */}
-  margin-top: 20px;
 `;
 const IotdCard = props => {
   console.log(props);
@@ -40,14 +39,22 @@ const IotdCard = props => {
           alt={props.title}
           style={{ maxWidth: "100%" }}
         />
-        <CardSubtitle style={{ fontSize: "1.25rem", paddingTop: "20px" }}>
+        <CardSubtitle
+          style={{
+            fontSize: "1.25rem",
+            paddingTop: "20px",
+            paddingBottom: "20px",
+          }}
+        >
           {props.explanation}
         </CardSubtitle>
         <Button
+          color="info"
           style={{
             width: "25%",
             marginLeft: "35%",
             marginTop: "20px",
+            marginBottom: "20px",
           }}
           onClick={() => window.open(props.hdurl, "_blank")}
         >
